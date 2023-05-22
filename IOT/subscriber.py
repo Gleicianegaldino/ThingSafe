41e29c52242a70f284217409b35a332d10b37bd3
Davi Souza de Luna <Sdavi738@gmail.com>
2023-05-22 01:15:50 -0300

    adicionando o status do dispositivo na API

--- IOT/subscriber.py
+++ IOT/subscriber.py
@@ -11,8 +11,8 @@ import os
 
 #COnfigurações do Banco
 host_banco="localhost"
-user_banco="thingsafe"
-passwd_banco="thingsafe"
+user_banco="root"
+passwd_banco="root"
 db_nome_banco="thingsafe"
 porta_banco = 3306
 tempo_espera_insert=1#provavelmente não será usado nesse código pois o insert será feito a cada iteração com o broker
@@ -55,6 +55,29 @@ Connected = False #Variável global utilizada como referência para saber se o s
 conexao = pymysql.connect(host=host_banco,port=porta_banco, user=user_banco, passwd=passwd_banco, db=db_nome_banco)
 cursor = conexao.cursor() #cursor agora é uma variável global
 
+
+#SE A FUNÇÃO FOR CHAMADA FORA DO ESCOPO IRÁ RETORNAR FALSE(DISPOSITIVO DESATIVADO) --> A função ainda não foi bem produzida
+def status_sensor_json_banco(DISPOSITIVO_TOPICO, DENTRO_OU_FORA_DA_FUNCAO, EH_JSON):
+    status = 1 if DENTRO_OU_FORA_DA_FUNCAO.lower() == "dentro" else 0
+
+    # Verificar se o registro já existe
+    select_query = "SELECT * FROM status WHERE DISPOSITIVO_TOPICO = %s"
+    cursor.execute(select_query, (DISPOSITIVO_TOPICO,))
+    existing_record = cursor.fetchone()
+
+    if existing_record is None:
+        # O registro não existe, realizar a inserção
+        insert_query = "INSERT INTO status(status_sensor, DISPOSITIVO_TOPICO) VALUES (%s, %s)"
+        cursor.execute(insert_query, (status, DISPOSITIVO_TOPICO))
+        conexao.commit()
+    else:
+        # O registro já existe, realizar o update
+        update_query = "UPDATE status SET status_sensor = %s WHERE DISPOSITIVO_TOPICO = %s"
+        cursor.execute(update_query, (status, DISPOSITIVO_TOPICO))
+        conexao.commit()
+   
+    
+
 def on_message(client, userdata, msg):
     
     print("=============================") 
@@ -70,7 +93,8 @@ def on_message(client, userdata, msg):
                 'mensagem': int(msg.payload),
                 'topico': str(msg.topic),
                 'qos': str(msg.qos),#Caso queira salvar como um inteiro você digita: 
-                'horario':now.isoformat()#guarda o horario no json
+                'horario':now.isoformat(),#guarda o horario no json
+                'status': status_sensor_json_banco(msg.topic,"Dentro",True)
                 };
             
             #parte dentro do laço que poderemos fazer o insert
@@ -82,6 +106,10 @@ def on_message(client, userdata, msg):
             topicoBanco= str(msg.topic)
             qosBanco= str(msg.qos)
             horario_formatado=datetime.datetime.now(datetime.timezone.utc)#formata o horario para esse formato para inserir no BD
+            
+            
+            status_sensor_json_banco(str(msg.topic),"Dentro",False)
+            
             
             #insert no BD
             cursor.execute(operacao_insert,(mensagemBanco,topicoBanco,qosBanco,horario_formatado))
