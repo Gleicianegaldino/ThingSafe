a605f291673afa719a94fd725f7714c81fd62d10
Davi Souza de Luna <Sdavi738@gmail.com>
2023-07-05 00:10:01 -0300

    feat:Tela de carregamento e mensagem caso não encontre nada

--- interface-web/resources/js/Pages/AnnualEvents.jsx
+++ interface-web/resources/js/Pages/AnnualEvents.jsx
@@ -4,41 +4,65 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
 import Footer from '@/Layouts/Footer';
 import EventList from '../Components/EventList';
+import { Oval } from 'react-loader-spinner';
 
 export default function DailyEvents({ auth }) {
-  const [events, setEvents] = useState([]);
+    const [events, setEvents] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-  const fetchEvents = async () => {
-    try {
-      const response = await axios.get('/events/year');//passando a rota com o parametro de dia
-      setEvents(response.data);
-    } catch (error) {
-      console.error('Failed to fetch daily events:', error);
-    }
-  };
+    const fetchEvents = async () => {
+        try {
+            const response = await axios.get('/events/year');
+            setEvents(response.data);
+            setLoading(false);
+        } catch (error) {
+            console.error('Failed to fetch daily events:', error);
+            setLoading(false);
+        }
+    };
 
-  useEffect(() => {
-    fetchEvents();
-  }, []);
+    useEffect(() => {
+        fetchEvents();
+    }, []);
 
-  return (
-    <>
-      <AuthenticatedLayout
-        user={auth.user}
-        header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Daily Events</h2>}
-      >
-        <Head title="Daily Events" />
-        <div className="py-12">
-          <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-              <div className="p-6 text-gray-900">
-                <EventList events={events} />
-              </div>
-            </div>
-          </div>
-        </div>
-      </AuthenticatedLayout>
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <AuthenticatedLayout
+                user={auth.user}
+                header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Daily Events</h2>}
+            >
+                <Head title="Daily Events" />
+                <div className="py-12">
+                    <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                        <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                            <div className="p-6 text-gray-900">
+                                {loading ? (
+                                    <div className="flex items-center justify-center">
+                                        <Oval
+                                            height={80}
+                                            width={80}
+                                            color="#ffff00"
+                                            wrapperStyle={{}}
+                                            wrapperClass=""
+                                            visible={true}
+                                            ariaLabel='oval-loading'
+                                            secondaryColor="#000000"
+                                            strokeWidth={2}
+                                            strokeWidthSecondary={2}
+
+                                        />
+                                    </div>
+                                ) : events.length > 0 ? (
+                                    <EventList events={events} />
+                                ) : (
+                                    <div className="text-center text-gray-500">Nenhum evento!</div>
+                                )}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </AuthenticatedLayout>
+            <Footer />
+        </>
+    );
 }
